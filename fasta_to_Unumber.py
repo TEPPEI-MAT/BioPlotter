b9611a25b84f40892f9ff73dbb7ecc665a39b7a2
TEPPEI-MAT <131732376+TEPPEI-MAT@users.noreply.github.com>
2025-01-21 14:45:40 +0900

    Update fasta_to_Unumber.py

--- fasta_to_Unumber.py
+++ fasta_to_Unumber.py
@@ -1,6 +1,6 @@
 import re
 
-with open('/Users/matsuda_teppei/idmapping_2024_11_24', 'r') as file:
+with open('', 'r') as file:
     data = file.read()
 
 entry_numbers = re.findall(r'tr\|([A-Z0-9]+)\|', data)
@@ -10,5 +10,5 @@ for entry in entry_numbers:
 
 
 entry_num = '\n'.join(entry_numbers)
-with open("/Users/matsuda_teppei/Documents/UAD/001_compre_gene/uniplot_num_3.txt" ,'wt') as f:
+with open("" ,'wt') as f:
     f.write(entry_num)
