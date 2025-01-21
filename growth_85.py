af52dee0c9fed73e5d9e98ad5dbd92a0595b187d
TEPPEI-MAT <131732376+TEPPEI-MAT@users.noreply.github.com>
2025-01-21 14:47:25 +0900

    Update growth_curve.py

--- growth_85.py
+++ growth_85.py
@@ -4,14 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-
-
-
 #define function#
 def IC50(x, a, b, c, d):
     return d + (a - d)/(1 + (x/c)**b)
-
-
 
 def graph_plot(max, a, b, c, d):
   limit = max + max/10
@@ -30,7 +25,7 @@ c=29.73884
 d=4166.04794
 
 #dataframe#
-df = pd.read_csv("/Users/matsuda_teppei/Documents/Nm6/122/WT_85.csv")
+df = pd.read_csv("tset.csv")
 df1 = df.groupby("conc",as_index = False,sort = False).describe()["dpm"]
 df2 = df.query("replicates == 'rep1'")
 df1["conc"] = df2["conc"]
@@ -52,12 +47,6 @@ x2 = df["conc"]
 y2 = df["dpm"]
 ax.errorbar(x1,y1,yerr = yerr,ecolor = "#000000",color = "#000000",capsize = 10,fmt = "go",markersize=10,capthick = 1,elinewidth = 1,zorder=2)
 
-
-
-
-
-
-
 plt.gca().spines['right'].set_visible(False)
 plt.gca().spines['top'].set_visible(False)
 plt.gca().yaxis.set_ticks_position('left')
@@ -67,51 +56,6 @@ ax.set_ylabel('Optical density (660 nm)',size=24)
 plt.yticks(size=25, color="#2b2b2b")
 plt.xticks(size=25, color="#2b2b2b")
 plt.xlim(2.5, 15)
-
-
-df = pd.read_csv("/Users/matsuda_teppei/Documents/Nm6/122/dTS_85.csv")
-df1 = df.groupby("conc",as_index = False,sort = False).describe()["dpm"]
-df2 = df.query("replicates == 'rep1'")
-df1["conc"] = df2["conc"]
-df3,cov = curve_fit(IC50,df["conc"],df["dpm"], p0=[  0.62057893, -11.05923388,   7.12642261,   0.09113962])
-xs, ys = graph_plot(df1["conc"].max()+1,df3[0],df3[1],df3[2],df3[3])
-df4 = pd.DataFrame({'conc': xs, 'dpm': ys})
-print(df3)
-
-x = df4["conc"]
-y = df4["dpm"]
-yerr = df1["std"]
-ax.plot(x,y,color = "#FF0000",lw =1.5,zorder=2)
-x1 = df1["conc"]
-y1 = df1["mean"]
-x2 = df["conc"]
-y2 = df["dpm"]
-ax.errorbar(x1,y1,yerr = yerr,ecolor = "#FF0000",color = "#FF0000",capsize = 10,fmt = "s",markersize=10,capthick = 1,elinewidth = 1,zorder=1)
-
-
-
-df = pd.read_csv("/Users/matsuda_teppei/Documents/Nm6/122/dTS_TS_85.csv")
-df1 = df.groupby("conc",as_index = False,sort = False).describe()["dpm"]
-df2 = df.query("replicates == 'rep1'")
-df1["conc"] = df2["conc"]
-df3,cov = curve_fit(IC50,df["conc"],df["dpm"], p0=[ 0.61774467, -8.3835741,   6.98319237,  0.08505468])
-xs, ys = graph_plot(df1["conc"].max()+1,df3[0],df3[1],df3[2],df3[3])
-df4 = pd.DataFrame({'conc': xs, 'dpm': ys})
-print(df3)
-
-x = df4["conc"]
-y = df4["dpm"]
-yerr = df1["std"]
-ax.plot(x,y,color = "#3333FF",lw =1.5,zorder=2)
-x1 = df1["conc"]
-y1 = df1["mean"]
-x2 = df["conc"]
-y2 = df["dpm"]
-ax.errorbar(x1,y1,yerr = yerr,ecolor = "#3333FF",color = "#3333FF",capsize = 10,fmt = "^",markersize=10,capthick = 1,elinewidth = 1,zorder=2)
-
-
-
-
 
 #plt.show()
 plt.savefig("/Users/matsuda_teppei/Library/CloudStorage/OneDrive-愛媛大学/Nm6_Matsuda/000_manuscript/figure/material/growth_85.png",dpi = 1200,bbox_inches='tight')
